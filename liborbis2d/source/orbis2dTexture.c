5e111ab1df76201957829b6982b6f3de96476b23
Ps3itaTeam <ps3itateam@ps3ita.it>
2018-07-08 18:50:42 +0200

    Added to orbis2dDrawTexture option to draw just a piece of the texture

--- liborbis2d/source/orbis2dTexture.c
+++ liborbis2d/source/orbis2dTexture.c
@@ -27,6 +27,7 @@ Orbis2dTexture * orbis2dCreateEmptyTexture(unsigned int w, unsigned int h)
 	}
 	return img;
 }
+
 void orbis2dDestroyTexture(Orbis2dTexture *texture)
 {
 	
@@ -39,15 +40,21 @@ void orbis2dDestroyTexture(Orbis2dTexture *texture)
 	}
 	free(texture);
 }
+
 uint32_t *orbis2dTextureGetDataPointer(Orbis2dTexture *texture)
 {
 	return texture->datap;
 }
+
 uint32_t orbis2dTextureGetStride(Orbis2dTexture *texture)
 {
 	return texture->width;
 }
-void orbis2dDrawTexture(Orbis2dTexture *texture, unsigned int x, unsigned int y)
+
+/* tx and ty are the coordinates in the texture, usefull if you want draw just a piece of texture, if not interested in that just set both to 0
+ * tw and th are the dimensions of texture, if you set them to 0 the whole image will be draw */
+
+void orbis2dDrawTexture(Orbis2dTexture *texture, uint32_t x, uint32_t y, uint32_t tx, uint32_t ty, uint32_t tw, uint32_t th)
 {
 	uint32_t *buf=NULL;
 	if(texture!=NULL)
@@ -55,7 +62,10 @@ void orbis2dDrawTexture(Orbis2dTexture *texture, unsigned int x, unsigned int y)
 		buf=orbis2dTextureGetDataPointer(texture);
 		if(buf!=NULL)
 		{
-			orbis2dPutImage2(buf,x,y,texture->width,texture->height);
+			if(tw==0 && th==0) {tw=texture->width; th=texture->height;}
+			if(tx+tw > texture->width) tw = (texture->width - tx);
+			if(ty+th > texture->height) th = (texture->height - ty);
+			orbis2dPutImage2(buf,x,y,tx,ty,tw,th,texture->width);
 		}
 	}
 }
