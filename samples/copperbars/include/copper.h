7c0a3306dd26752213569b8713d26937a9d2f424
masterzorag <masterzorag@gmail.com>
2018-07-22 11:14:48 +0200

    new sample: copperbars

---
+++ samples/copperbars/include/copper.h
@@ -0,0 +1,10 @@
+#ifndef __COPPER_H__
+#define __COPPER_H__
+
+#include <sys/types.h>
+
+
+void init_copperbars(void);
+void draw_copperbars(void);
+
+#endif // __COPPER_H__
---
+++ samples/copperbars/include/scroller.h
@@ -0,0 +1,9 @@
+#ifndef __SCROLLER_H__
+#define __SCROLLER_H__
+
+
+void init_sinetext(void);
+void draw_sinetext(const int y);
+void move_sinetext(void);
+
+#endif // __SCROLLER_H__
