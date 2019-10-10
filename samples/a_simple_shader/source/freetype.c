85ff34827476afd88e6497a5cb51bcb0fdf07d41
masterzorag <masterzorag@gmail.com>
2019-10-10 20:17:25 +0200

    freetype.c update
    - reuse framebuffer pointer, get rid of orbis2dDrawPixelColor() calls
    - introduce a pthread mutex lock
    - update bounds check

--- samples/a_simple_shader/source/freetype.c
+++ samples/a_simple_shader/source/freetype.c
@@ -8,6 +8,8 @@
 #include <math.h>       // sinf, cosf
 #include <sys/fcntl.h>  // O_RDONLY
 #include <orbis2d.h>
+extern Orbis2dConfig *orbconf;  // hook main 2d conf to get framebuffer address
+static uint32_t __attribute__((aligned(16))) *pixel = NULL;
 
 ///TTF
 #include <ft2build.h>
@@ -18,6 +20,7 @@ static FT_Face    face = NULL;
 static FT_Byte *buffer = NULL;  // stores ttf font data
 static size_t   bufsize;
 
+/* precomputed gradient colors [0-7] */
 #define SIMPLE_GRADIENT_STEPS 8
 /*static uint32_t fading_color[8] = { 0xFFFF0000, 0xFF0000FF, 0xFFFF0000, 0xFF0000FF,
                                       0xFFFF0000, 0xFF0000FF, 0xFFFF0000, 0xFF0000FF };*/
@@ -29,10 +32,10 @@ static uint32_t c1 = 0xFFFF0000,
 /***********************************************************************
 * simple gradient (ARGB)
 *
-* uint32_t *a     = pointer to start color
-* uint32_t *b     = pointer to end color
-* uint8_t steps   = number of steps we split fade
-* uint8_t step    = which step we compute and return
+* uint32_t *a   = pointer to start color
+* uint32_t *b   = pointer to end color
+* uint8_t steps = number of steps we split fade
+* uint8_t step  = which step we compute and return
 ***********************************************************************/
 typedef float float4 __attribute__((ext_vector_type(4)));
 
@@ -58,8 +61,8 @@ static uint32_t simple_gradient(const uint32_t *a, const uint32_t *b, const uint
 *
 * precompute palette to use in print_text() and setup colors range
 *
-* uint32_t *a     = pointer to start color
-* uint32_t *b     = pointer to end color
+* uint32_t *a = pointer to start color
+* uint32_t *b = pointer to end color
 ***********************************************************************/
 void FT_update_gradient(const uint32_t *a, const uint32_t *b)
 {
@@ -111,38 +114,40 @@ static uint8_t *buffer_from_host(const char *path, size_t *ret_size)
 	return buf;
 }
 
-// internally draw to liborbis2d
-static void draw_bitmap( FT_Bitmap *bitmap, FT_Int x, FT_Int y)      // glyph refs
+/* internally draw via liborbis2d */
+static void draw_bitmap(FT_Bitmap *bitmap, FT_Int x, FT_Int y) // glyph refs
 {
-    uint32_t pixelColor = 0xFF000000;
+    //uint32_t pixelColor = 0xFF000000;
     uint8_t c;
-    
+
     FT_Int i, j, p, q;
     FT_Int x_max = x + bitmap->width;
     FT_Int y_max = y + bitmap->rows;
-//debugNetPrintf(DEBUG,"\n%d %d %d %d\n", x, x_max, y, y_max);
+
     /* for simplicity, we assume that `bitmap->pixel_mode' */
     /* is `FT_PIXEL_MODE_GRAY' (i.e., not a bitmap font)   */
 
-    for(i = x, p = 0; i < x_max; i++, p++ )     
+    for(i = x, p = 0; i < x_max; i++, p++ )
     {
         if(i < 0 || i >= ATTR_WIDTH) continue;
 
-        for(j = y, q = 0; j < y_max; j++, q++ )     
+        for(j = y, q = 0; j < y_max; j++, q++ )
         {
             if(j < 0 || j >= ATTR_HEIGHT ) continue;
-        
+
             //image[j][i] |= bitmap->buffer[q * bitmap->width + p];
             c = bitmap->buffer[q * bitmap->width + p];
-          
+
             /* we can do better about color */
             if(c > 0
             && c < 0x80) {
                 //pixelColor = ARGB(0xFF, c, c, c);
-                orbis2dDrawPixelColor(i, j, 0xFF808080);
+                //orbis2dDrawPixelColor(i, j, 0xFF808080);
+                pixel[j * ATTR_WIDTH + i] = 0xFF808080;
             } else if(c > 0x80) {
                 //pixelColor = ARGB(0xFF, 0x10 + c + q*2, 0x10 + c + q*2, 0x10 + c + q*2);
-                orbis2dDrawPixelColor(i, j, fading_color[q /3]);
+                //orbis2dDrawPixelColor(i, j, fading_color[q /3]);
+                pixel[j * ATTR_WIDTH + i] = fading_color[q /3];
             }
         }
     }
@@ -157,58 +162,63 @@ void add_angle(void)
     float a = angle;
     a += 0.25;
     if(a >= 360) a = 0;
-    if(a < 0) a = 360;
+    if(a < 0)    a = 360;
     angle = a;
     //debugNetPrintf(DEBUG,"angle: %.f\n", angle);
 }
 
-//new glyph sequence loader
+/* new glyph sequence loader */
+#include <pthread.h>
+pthread_mutex_t lock;
 
-
-
-// print from main()
+// print from main() or some pthread
 void FT_print_text(uint dst_x, uint dst_y, const char *text)
 {
     if(!face) return;
 
+    pthread_mutex_lock(&lock);
+
     int num_chars = strlen(text);
     FT_GlyphSlot  slot = face->glyph;   /* a small shortcut */
-    FT_Vector     pen;                  /* untransformed origin  */
+    FT_Vector     pen;                  /* untransformed origin */
 
     /* the pen position in 26.6 cartesian space coordinates; */
-    /* start at (dst_x,dst_y) relative to the upper left corner  */
+    /* start at (dst_x, dst_y) relative to the upper left corner  */
     pen.x = dst_x * 64;
     pen.y = ( ATTR_HEIGHT - dst_y ) * 64;
 
     /* transformation matrix */
-    FT_Matrix     matrix;                 
-    float a = ( angle / 360 ) * 3.14159 * 2;  // use of degrees
+    FT_Matrix matrix;
+    float     a = ( angle / 360 ) * 3.14159 * 2;  // use of degrees
     /* set up matrix */
     matrix.xx = (FT_Fixed)( cosf( a ) * 0x10000L );
     matrix.xy = (FT_Fixed)(-sinf( a ) * 0x10000L );
     matrix.yx = (FT_Fixed)( sinf( a ) * 0x10000L );
     matrix.yy = (FT_Fixed)( cosf( a ) * 0x10000L );
 
+    /* refresh current fb address */
+    pixel = (uint32_t *)orbconf->surfaceAddr[orbconf->currentBuffer];
+
     for(uint n = 0; n < num_chars; n++)
     {
         /* set transformation */
         FT_Set_Transform( face, &matrix, &pen );
-    
+
         /* load glyph image into the slot (erase previous one) */
         FT_Load_Char( face, text[n], FT_LOAD_RENDER );
         /* ignore errors */
-    
+
         /* now, draw to our target surface (convert position) */
-        draw_bitmap(
-                    &slot->bitmap,
+        draw_bitmap(&slot->bitmap,
                      slot->bitmap_left,
                      ATTR_HEIGHT - slot->bitmap_top);
-    
+
         /* increment pen position */
         pen.x += slot->advance.x;
         pen.y += slot->advance.y;
         //debugNetPrintf(DEBUG,"[%d] %d %d %d %d\n", n, slot->bitmap_left, slot->bitmap_top, pen.x /64, pen.y /64);
     }
+    pthread_mutex_unlock(&lock);
 }
 
 
@@ -219,17 +229,17 @@ void FT_print_text(uint dst_x, uint dst_y, const char *text)
 void FT_init()
 {
     FT_update_gradient(&c1, &c2);
-    
+
     FT_Error error;
 
     /* initialize library */
-    error = FT_Init_FreeType( &library );              
+    error = FT_Init_FreeType( &library );
     /* error handling omitted */
     debugNetPrintf(DEBUG,"FT_Init_FreeType return:\t%d, library at %p\n", error, library);
-    
+
     /* load default font */
     if(!buffer) buffer = buffer_from_host("host0:main.ttf", &bufsize);
-    
+
     if(!buffer) return;
     // passed, report
     debugNetPrintf(DEBUG,"data buffer at %p, size %u\n", buffer, bufsize);
@@ -245,16 +255,21 @@ void FT_init()
         /* error handling omitted */
         debugNetPrintf(DEBUG,"FT_New_Memory_Face return:\t%d, face at:\t%p\n", error, face);
     }
-  
-    /* use 10pt at 100dpi */
-    error = FT_Set_Char_Size( face, 10 * 64, 0, 100, 0 );    /* set character size */
+
+    /* use 14pt at 100dpi */
+    error = FT_Set_Char_Size( face, 14 * 64, 0, 100, 0 );    /* set character size */
     /* error handling omitted */
     debugNetPrintf(DEBUG,"FT_Set_Char_Size return:\t%d\n", error);
-      
+
     /* cmap selection omitted;                                        */
     /* for simplicity we assume that the font contains a Unicode cmap */
 
-    // retunr NOW, follows an example
+    if(pthread_mutex_init(&lock, NULL) != 0)
+    {
+        debugNetPrintf(ERROR,"\n mutex init failed\n");
+    }
+
+    // return NOW, follows an example
     return;
 
 // from example1
@@ -285,17 +300,17 @@ void FT_init()
       {
         /* set transformation */
         FT_Set_Transform( face, &matrix, &pen );
-    
+
         /* load glyph image into the slot (erase previous one) */
         error = FT_Load_Char( face, text[n], FT_LOAD_RENDER );
         if ( error )
           continue;                 /* ignore errors */
-    
+
         /* now, draw to our target surface (convert position) */
-        /*draw_bitmap( &slot->bitmap,
-                     slot->bitmap_left,
-                     target_height - slot->bitmap_top );*/
-    
+        /*draw_bitmap(&slot->bitmap,
+                      slot->bitmap_left,
+                      target_height - slot->bitmap_top );*/
+
         /* increment pen position */
         pen.x += slot->advance.x;
         pen.y += slot->advance.y;
@@ -303,17 +318,19 @@ void FT_init()
 }
 
 
-// cleanup freetype
+/* cleanup freetype */
 void FT_end(void)
 {
     FT_Error error;
-      
+
     error = FT_Done_Face( face );
     debugNetPrintf(DEBUG,"FT_Done_Face return:\t%d\n", error);
-     
+
     error = FT_Done_FreeType( library );
-    debugNetPrintf(DEBUG,"FT_Done_FreeType return:\t%d\n", error);     
-     
+    debugNetPrintf(DEBUG,"FT_Done_FreeType return:\t%d\n", error);
+
+    pthread_mutex_destroy(&lock);
+
     // release data buffer (readed from host0)
     if(buffer) { free(buffer), buffer = NULL; }
 }
